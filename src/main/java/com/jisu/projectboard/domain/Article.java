fcf33acb131f1f170b311e79216d4b3f50b32375
jisu3316 <jisu3316@naver.com>
2022-07-07 20:38:58 +0900

    #12 - AuditingFields 추출
    생성자, 생성일시, 수정자, 수정일시는 반복적으로
    엔티티 클래스에 들어가는 요소이고, 
    도메인과 직접 연관이 없는 요소이므로 추출이 가능하다.
    `@MappedSuperclass` 이용하여 상속 방식으로 추출함.
    그외 `@DateTimeFormat` 요소 추가

--- src/main/java/com/jisu/projectboard/domain/Article.java
+++ src/main/java/com/jisu/projectboard/domain/Article.java
@@ -24,28 +24,28 @@ import java.util.Set;
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy")
 })
-@EntityListeners(AuditingEntityListener.class)
 @Entity
-public class Article {
+public class Article extends AuditingFields{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Setter @Column(nullable = false) private String title;  // 제목
-    @Setter @Column(nullable = false, length = 10000) private String content;  // 내용
+    @Setter
+    @Column(nullable = false)
+    private String title;  // 제목
 
-    @Setter private String hashtag; // 해시태그
+    @Setter
+    @Column(nullable = false, length = 10000)
+    private String content;  // 내용
+
+    @Setter
+    private String hashtag; // 해시태그
 
     @OrderBy("id")
     @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
     @ToString.Exclude
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
-
-    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; // 생성일시
-    @CreatedBy @Column(nullable = false, length = 100) private String createdBy; // 생성자
-    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; // 수정일시
-    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy; // 수정자
 
     protected Article() {}
 
@@ -54,6 +54,7 @@ public class Article {
         this.content = content;
         this.hashtag = hashtag;
     }
+
     public static Article of(String title, String content, String hashtag) {
         return new Article(title, content, hashtag);
     }
