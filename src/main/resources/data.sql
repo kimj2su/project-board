927f270d556084df63d9f1c956fa3f98ac928996
jisu3316 <jisu3316@naver.com>
2022-07-07 18:36:10 +0900

    #10 - 테스트 데이터를 data.sql로 추가
    이걸로 테스트 통과

---
+++ src/main/resources/data.sql
@@ -0,0 +1,41 @@
+insert into article
+(title, content, hashtag, created_by, modified_by, modified_at, created_at)
+    values
+    ('제목1', '내용1', '#', 'jisu', 'jisu', now(), now());
+insert into article
+(title, content, hashtag, created_by, modified_by, modified_at, created_at)
+values
+    ('제목1', '내용1', '#', 'jisu', 'jisu', now(), now());
+insert into article
+(title, content, hashtag, created_by, modified_by, modified_at, created_at)
+values
+    ('제목1', '내용1', '#', 'jisu', 'jisu', now(), now());
+insert into article
+(title, content, hashtag, created_by, modified_by, modified_at, created_at)
+values
+    ('제목1', '내용1', '#', 'jisu', 'jisu', now(), now());
+insert into article
+(title, content, hashtag, created_by, modified_by, modified_at, created_at)
+values
+    ('제목1', '내용1', '#', 'jisu', 'jisu', now(), now());
+
+insert into article_comment
+(content,article_id, created_by, modified_by, modified_at, created_at)
+values
+    ('댓글', 1, 'jisu' , 'jisu', now(), now());
+insert into article_comment
+(content,article_id, created_by, modified_by, modified_at, created_at)
+values
+    ('댓글', 2, 'jisu' , 'jisu', now(), now());
+insert into article_comment
+(content,article_id, created_by, modified_by, modified_at, created_at)
+values
+    ('댓글', 3, 'jisu' , 'jisu', now(), now());
+insert into article_comment
+(content,article_id, created_by, modified_by, modified_at, created_at)
+values
+    ('댓글', 4, 'jisu' , 'jisu', now(), now());
+insert into article_comment
+(content,article_id, created_by, modified_by, modified_at, created_at)
+values
+    ('댓글', 5, 'jisu' , 'jisu', now(), now());
