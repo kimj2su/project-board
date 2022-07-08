ee01cb6e11175fed1cd045e9683e482048896ad6
jisu3316 <jisu3316@naver.com>
2022-07-08 14:32:47 +0900

    #1 - data rest 설정
    게시글, 댓글의 json api를 자동으로 
    restfil 하게 만들게끔 설정

--- src/main/java/com/jisu/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/jisu/projectboard/repository/ArticleCommentRepository.java
@@ -2,7 +2,10 @@ package com.jisu.projectboard.repository;
 
 import com.jisu.projectboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
+
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
--- src/main/java/com/jisu/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/jisu/projectboard/repository/ArticleRepository.java
@@ -2,7 +2,9 @@ package com.jisu.projectboard.repository;
 
 import com.jisu.projectboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
