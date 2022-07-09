70bbc060b521a51b6c54a56b548fe9706f7c3275
jisu3316 <jisu3316@naver.com>
2022-07-09 18:47:26 +0900

    #1 - Querydsl 지원을 이용하여 api 검색 옵션 추가
    `QuerydslPredicateExecutor`,
    `QuerydslBinderCustomizer` 를 이용하여 간단하게 개발

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,19 +5,100 @@ insert into article
 insert into article
 (title, content, hashtag, created_by, modified_by, modified_at, created_at)
 values
-    ('제목1', '내용1', '#', 'jisu', 'jisu', now(), now());
+    ('제목2', '내용2', '#', 'jisu', 'jisu', now(), now());
 insert into article
 (title, content, hashtag, created_by, modified_by, modified_at, created_at)
 values
-    ('제목1', '내용1', '#', 'jisu', 'jisu', now(), now());
+    ('제목3', '내용3', '#', 'jisu', 'jisu', now(), now());
 insert into article
 (title, content, hashtag, created_by, modified_by, modified_at, created_at)
 values
-    ('제목1', '내용1', '#', 'jisu', 'jisu', now(), now());
+    ('제목4', '내용4', '#', 'jisu', 'jisu', now(), now());
 insert into article
 (title, content, hashtag, created_by, modified_by, modified_at, created_at)
 values
-    ('제목1', '내용1', '#', 'jisu', 'jisu', now(), now());
+    ('제목5', '내용5', '#', 'jisu', 'jisu', now(), now());
+insert into article
+(title, content, hashtag, created_by, modified_by, modified_at, created_at)
+values
+    ('제목6', '내용6', '#', 'jisu', 'jisu', now(), now());
+insert into article
+(title, content, hashtag, created_by, modified_by, modified_at, created_at)
+values
+    ('제목7', '내용7', '#', 'jisu', 'jisu', now(), now());
+insert into article
+(title, content, hashtag, created_by, modified_by, modified_at, created_at)
+values
+    ('제목8', '내용8', '#', 'jisu', 'jisu', now(), now());
+insert into article
+(title, content, hashtag, created_by, modified_by, modified_at, created_at)
+values
+    ('제목9', '내용9', '#', 'jisu', 'jisu', now(), now());
+insert into article
+(title, content, hashtag, created_by, modified_by, modified_at, created_at)
+values
+    ('제목10', '내용10', '#', 'jisu', 'jisu', now(), now());
+insert into article
+(title, content, hashtag, created_by, modified_by, modified_at, created_at)
+values
+    ('제목11', '내용11', '#', 'jisu', 'jisu', now(), now());
+insert into article
+(title, content, hashtag, created_by, modified_by, modified_at, created_at)
+values
+    ('제목12', '내용12', '#', 'jisu', 'jisu', now(), now());
+insert into article
+(title, content, hashtag, created_by, modified_by, modified_at, created_at)
+values
+    ('제목13', '내용13', '#', 'jisu', 'jisu', now(), now());
+insert into article
+(title, content, hashtag, created_by, modified_by, modified_at, created_at)
+values
+    ('제목14', '내용14', '#', 'jisu', 'jisu', now(), now());
+insert into article
+(title, content, hashtag, created_by, modified_by, modified_at, created_at)
+values
+    ('제목15', '내용15', '#', 'jisu', 'jisu', now(), now());
+insert into article
+(title, content, hashtag, created_by, modified_by, modified_at, created_at)
+values
+    ('제목16', '내용16', '#', 'jisu', 'jisu', now(), now());
+insert into article
+(title, content, hashtag, created_by, modified_by, modified_at, created_at)
+values
+    ('제목17', '내용17', '#', 'jisu', 'jisu', now(), now());
+insert into article
+(title, content, hashtag, created_by, modified_by, modified_at, created_at)
+values
+    ('제목18', '내용18', '#', 'jisu', 'jisu', now(), now());
+insert into article
+(title, content, hashtag, created_by, modified_by, modified_at, created_at)
+values
+    ('제목19', '내용19', '#', 'jisu', 'jisu', now(), now());
+insert into article
+(title, content, hashtag, created_by, modified_by, modified_at, created_at)
+values
+    ('제목20', '내용20', '#', 'jisu', 'jisu', now(), now());
+insert into article
+(title, content, hashtag, created_by, modified_by, modified_at, created_at)
+values
+    ('제목21', '내용21', '#', 'jisu', 'jisu', now(), now());
+insert into article
+(title, content, hashtag, created_by, modified_by, modified_at, created_at)
+values
+    ('제목22', '내용22', '#', 'jisu', 'jisu', now(), now());
+insert into article
+(title, content, hashtag, created_by, modified_by, modified_at, created_at)
+values
+    ('제목23', '내용23', '#', 'jisu', 'jisu', now(), now());
+insert into article
+(title, content, hashtag, created_by, modified_by, modified_at, created_at)
+values
+    ('제목24', '내용24', '#', 'jisu', 'jisu', now(), now());
+insert into article
+(title, content, hashtag, created_by, modified_by, modified_at, created_at)
+values
+    ('제목25', '내용25', '#', 'jisu', 'jisu', now(), now());
+
 
 insert into article_comment
 (content,article_id, created_by, modified_by, modified_at, created_at)
@@ -39,3 +120,63 @@ insert into article_comment
 (content,article_id, created_by, modified_by, modified_at, created_at)
 values
     ('댓글', 5, 'jisu' , 'jisu', now(), now());
+insert into article_comment
+(content,article_id, created_by, modified_by, modified_at, created_at)
+values
+    ('댓글', 6, 'jisu' , 'jisu', now(), now());
+insert into article_comment
+(content,article_id, created_by, modified_by, modified_at, created_at)
+values
+    ('댓글', 7, 'jisu' , 'jisu', now(), now());
+insert into article_comment
+(content,article_id, created_by, modified_by, modified_at, created_at)
+values
+    ('댓글', 8, 'jisu' , 'jisu', now(), now());
+insert into article_comment
+(content,article_id, created_by, modified_by, modified_at, created_at)
+values
+    ('댓글', 9, 'jisu' , 'jisu', now(), now());
+insert into article_comment
+(content,article_id, created_by, modified_by, modified_at, created_at)
+values
+    ('댓글', 10, 'jisu' , 'jisu', now(), now());
+insert into article_comment
+(content,article_id, created_by, modified_by, modified_at, created_at)
+values
+    ('댓글', 1, 'jisu' , 'jisu', now(), now());
+insert into article_comment
+(content,article_id, created_by, modified_by, modified_at, created_at)
+values
+    ('댓글', 2, 'jisu' , 'jisu', now(), now());
+insert into article_comment
+(content,article_id, created_by, modified_by, modified_at, created_at)
+values
+    ('댓글', 3, 'jisu' , 'jisu', now(), now());
+insert into article_comment
+(content,article_id, created_by, modified_by, modified_at, created_at)
+values
+    ('댓글', 4, 'jisu' , 'jisu', now(), now());
+insert into article_comment
+(content,article_id, created_by, modified_by, modified_at, created_at)
+values
+    ('댓글', 5, 'jisu' , 'jisu', now(), now());
+insert into article_comment
+(content,article_id, created_by, modified_by, modified_at, created_at)
+values
+    ('댓글', 6, 'jisu' , 'jisu', now(), now());
+insert into article_comment
+(content,article_id, created_by, modified_by, modified_at, created_at)
+values
+    ('댓글', 7, 'jisu' , 'jisu', now(), now());
+insert into article_comment
+(content,article_id, created_by, modified_by, modified_at, created_at)
+values
+    ('댓글', 8, 'jisu' , 'jisu', now(), now());
+insert into article_comment
+(content,article_id, created_by, modified_by, modified_at, created_at)
+values
+    ('댓글', 9, 'jisu' , 'jisu', now(), now());
+insert into article_comment
+(content,article_id, created_by, modified_by, modified_at, created_at)
+values
+    ('댓글', 10, 'jisu' , 'jisu', now(), now());
