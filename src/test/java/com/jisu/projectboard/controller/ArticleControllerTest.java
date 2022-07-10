b97008c5c76882703db3ae97fc12e11cb3a01a60
jisu3316 <jisu3316@naver.com>
2022-07-10 15:48:10 +0900

    #16 - 게시판 뷰 기본 틀 작성
    컨트롤러가 테스트를 통과하게 만들어주고
    뷰 적절히 뼈대 잡았음.
    테스트에서는 media type 검사에서 부가적인 옵션이 들어갈 수 있음을 확인했기 떄문에
    적절히 테스트 메서드 수정

--- src/test/java/com/jisu/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/jisu/projectboard/controller/ArticleControllerTest.java
@@ -25,7 +25,7 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현중")
+
     @DisplayName("view - get 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticleView_thenReturnsArticlesView() throws  Exception {
@@ -34,7 +34,7 @@ class ArticleControllerTest {
         // when & then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
     }
@@ -50,7 +50,7 @@ class ArticleControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
-                .andExpect(model().attributeExists("article"));
+                .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
     }
 
