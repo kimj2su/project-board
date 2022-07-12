79a8c999f56f5563c720fda33ebaa9230e95d1c7
jisu3316 <jisu3316@naver.com>
2022-07-12 20:12:09 +0900

    #20 - 루트 경로를 게시판 페이지로 forward 하도록 컨트롤러 구현
    IndexController 를 만들어서 
    기본 페이지가 게시판 페이지가 되도록함

---
+++ src/test/java/com/jisu/projectboard/controller/IndexControllerTest.java
@@ -0,0 +1,40 @@
+package com.jisu.projectboard.controller;
+
+import com.jisu.projectboard.config.SecurityConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Import(SecurityConfig.class)
+@WebMvcTest(IndexController.class)
+class IndexControllerTest {
+
+    private final MockMvc mvc;
+
+    public IndexControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @Test
+    void givenNothing_whenRequestingIndexPage_thenRedirectsToArticlesPage() throws Exception {
+        // given
+
+        // when & then
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("forward:/articles"))
+                .andExpect(forwardedUrl("/articles"))
+                .andDo(MockMvcResultHandlers.print());
+
+    }
+
+}
--- src/test/java/com/jisu/projectboard/service/ArticleCommentServiceTest.java
+++ src/test/java/com/jisu/projectboard/service/ArticleCommentServiceTest.java
@@ -1,22 +1,25 @@
 package com.jisu.projectboard.service;
 
 import com.jisu.projectboard.domain.Article;
+import com.jisu.projectboard.domain.ArticleComment;
 import com.jisu.projectboard.dto.ArticleCommentDto;
 import com.jisu.projectboard.repository.ArticleCommentRepository;
 import com.jisu.projectboard.repository.ArticleRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 
 @DisplayName("비즈니스 로직 - 댓글")
 @ExtendWith(MockitoExtension.class)
@@ -42,22 +45,19 @@ class ArticleCommentServiceTest {
 
         //then
         assertThat(articleComments).isNotNull();
-        BDDMockito.then(articleRepository).should().findById(articleId);
+        then(articleRepository).should().findById(articleId);
     }
 
     @DisplayName("댓글 정보를 입력하면, 댓글을 저장한다.")
     @Test
     void givenArticleCommentInfo_whenSavingArticleComment_thenSavesArticleComment() {
-        //given
-        Long articleId = 1L;
+        // Given
+        given(articleCommentRepository.save(any(ArticleComment.class))).willReturn(null);
 
-        given(articleRepository.findById(articleId)).willReturn(Optional.of(Article.of("title", "content", "#java")));
+        // When
+        sut.saveArticleComment(ArticleCommentDto.of(LocalDateTime.now(), "Jisu", LocalDateTime.now(), "Jisu", "comment"));
 
-        //when
-        List<ArticleCommentDto> articleComments = sut.searchArticleComment(articleId);
-
-        //then
-        assertThat(articleComments).isNotNull();
-        BDDMockito.then(articleRepository).should().findById(articleId);
+        // Then
+        then(articleCommentRepository).should().save(any(ArticleComment.class));
     }
 }
