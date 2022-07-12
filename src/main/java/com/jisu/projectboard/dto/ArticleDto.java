7177f55a29220a7472b396999ca44a9a5da06ca3
jisu3316 <jisu3316@naver.com>
2022-07-12 20:02:38 +0900

    #20 - 게시글 서비스 로직의 테스트와 골격 잡기
    다음의 기능을 테스트로 표현
    
    * 게시글 검색하면 게시글 리스트 반환
    * 게시글을 ID로 조회
    * 게시글 생성
    * 게시글 수정
    * 게시글 삭제
    
    필요한 dto 와 enum을 함께 작성했는데,
    추후에 수정할 수도 있음

---
+++ src/main/java/com/jisu/projectboard/dto/ArticleDto.java
@@ -0,0 +1,19 @@
+package com.jisu.projectboard.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ArticleDto {
+
+    private final LocalDateTime createdAt;
+    private final String createdBy;
+    private final String title;
+    private final String content;
+    private final String hashtag;
+
+    public static ArticleDto of(LocalDateTime createdAt, String createdBy, String title, String content, String hashtag) {
+        return new ArticleDto(createdAt, createdBy, title, content, hashtag);
+    }
+}
---
+++ src/main/java/com/jisu/projectboard/service/ArticleService.java
@@ -0,0 +1,42 @@
+package com.jisu.projectboard.service;
+
+import com.jisu.projectboard.domain.type.SearchType;
+import com.jisu.projectboard.dto.ArticleDto;
+import com.jisu.projectboard.dto.ArticleUpdateDto;
+import com.jisu.projectboard.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+
+    @Transactional(readOnly = true)
+    public Page<ArticleDto> searchArticles(SearchType title, String search_keyword) {
+        return Page.empty();
+    }
+
+    @Transactional(readOnly = true)
+    public ArticleDto searchArticle(long id) {
+        return null;
+    }
+
+    public void saveArticle(ArticleDto dto) {
+
+    }
+
+    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+
+    }
+
+    public void deleteArticle(long articleId) {
+
+    }
+}
